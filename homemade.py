c974e3cc486d75369ed8776a72368462c1f361b5
Oliver Tzeng <olivertzeng@proton.me>
2025-03-05 18:49:55 +0800

    feat(anarchychess): holy hell

--- homemade.py
+++ homemade.py
@@ -14,10 +14,13 @@ from lib.engine_wrapper import MinimalEngine
 from lib.lichess_types import HOMEMADE_ARGS_TYPE
 
 logger = logging.getLogger(__name__)
-stockfishPath = "stockfish"
 
 
-class Anarchy(MinimalEngine):
+class ExampleEngine(MinimalEngine):
+    """An example engine that all homemade engines inherit."""
+
+
+class Anarchy(ExampleEngine):
 
     def search(
         self, board: chess.Board, time_limit: Limit, *args: HOMEMADE_ARGS_TYPE
@@ -27,7 +30,6 @@ class Anarchy(MinimalEngine):
         legalMoves = list(board.legal_moves)
 
         # Initialise variables
-        bestEvaluation = None
         bestMove = None
 
         # Evaluate each move
@@ -41,38 +43,26 @@ class Anarchy(MinimalEngine):
             # play the ruy lopez
             if board.board_fen() == "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR":
                 print("e4 best by test")
-                return "e2e4"
+                return PlayResult(chess.Move.from_uci("e2e4"), None)
 
             if board.board_fen() == "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR":
-                return "g1f3"
+                return PlayResult(chess.Move.from_uci("q1f3"), None)
 
             if (
                 board.board_fen()
                 == "r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R"
             ):
-                return PlayResult("f1b5", None)
+                return PlayResult(chess.Move.from_uci("f1b5"), None)
 
             # always play the bongcloud
-            if (
-                board.san(move) == "Ke2"
-                or board.san(move) == "Ke7"
-                or board.san(move) == "Kxe2"
-                or board.san(move) == "Kxe7"
-            ):
+            if board.san(move) in ["Ke2", "Ke7", "Kxe2", "Kxe7"]:
                 return PlayResult(move, None)
 
             # never play rook a4
             if not (board.san(move)[0] == "R" and board.san(move)[-2:] == "a4"):
                 # king stays on e2/e7
                 if not (board.san(move)[0] == "K"):
-                    # play move
                     board.push(move)
-                    # evaluate position
-                    evaluation = self.evaluate(board, searchTime)
-                    # if the evaluation is better than the current position use it as the new best move
-                    if bestEvaluation is None or bestEvaluation > evaluation:
-                        bestEvaluation = evaluation
-                        bestMove = move
                     board.pop()
             else:
                 print("I saw Ra4, I just didn't like it")
